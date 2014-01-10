1f291071acf0746d8c2dd949961a15a012ec8699
jshou <joshuahou@gmail.com>
2014-01-10 11:13:13 -0800

    dude can jump

---
+++ Assets/Scripts/DudeController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DudeController : MonoBehaviour {
+  public Rigidbody2D Dude;
+  public Transform DudeTransform;
+  public float JumpForce = 500f;
+
+  private const float GROUND_HEIGHT = -2.65f;
+
+  void Update () {
+    if(Input.GetButtonDown("Jump") && DudeTransform.localPosition.y < GROUND_HEIGHT) {
+      Dude.AddForce(new Vector2(0f, JumpForce));
+    }
+  }
+}
