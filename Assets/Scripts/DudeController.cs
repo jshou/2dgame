9833e75d077f59352d3e227c3c5c54619c208bfb
jshou <joshuahou@gmail.com>
2014-01-10 13:52:21 -0800

    add a platform

--- Assets/Scripts/DudeController.cs
+++ Assets/Scripts/DudeController.cs
@@ -7,14 +7,14 @@ public class DudeController : MonoBehaviour {
   public float JumpForce = 500f;
 
   private const float GROUND_HEIGHT = -2.65f;
-  private const float WALK_SPEED = 0.1f;
+  private const float WALK_SPEED = 10f;
 
   void Update () {
     if (WellGrounded()) {
       if(Input.GetButtonDown("Jump")) {
         Dude.AddForce(new Vector2(0f, JumpForce));
       } else if (Input.GetButton("Horizontal")) {
-        Dude.AddForce(new Vector2(Input.GetAxis("Horizontal") * 10, 0f));
+        Dude.AddForce(new Vector2(Input.GetAxis("Horizontal") * WALK_SPEED, 0f));
       } else if (Input.GetButtonUp("Horizontal")) {
         Dude.velocity = Vector2.zero;
       }
